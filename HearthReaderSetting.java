3f0de08e726cf3f2f285867e4e6d3c5772a52c31
Evert Chin <evertchin@gmail.com>
2013-10-30 20:00:39 +0800

    Tuned minScore to match winsNumbers better in English mode.

--- HearthReaderSetting.java
+++ HearthReaderSetting.java
@@ -1,5 +1,5 @@
 public class HearthReaderSetting {
 	public String lang = "enUS";
-	public float winsNumberMatchQuality = 0.4f;
+	public float winsNumberMatchQuality = 0.5f;
 	public float winsNumberScaleFactor = 1f;
 }
