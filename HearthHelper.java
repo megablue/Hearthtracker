403d222144b3e41e2cecf559c0d79966f83dd24a
Evert Chin <evertchin@gmail.com>
2013-10-31 15:33:41 +0800

    Added dynamic class loader to handle swt platform dependecies issues.

--- HearthHelper.java
+++ HearthHelper.java
@@ -2,6 +2,9 @@ import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.Method;
+import java.net.URL;
+import java.net.URLClassLoader;
 import java.nio.ByteBuffer;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
@@ -56,6 +59,71 @@ public class HearthHelper {
 		}
 		return null;
 	}
+	
+	public static String getArchFilename(String prefix) 
+	{ 
+	   return prefix + "_" + getOSName() + "_" + getArchName() + ".jar"; 
+	} 
 
+	public static String getOSName() 
+	{ 
+	   String osNameProperty = System.getProperty("os.name"); 
 
+	   if (osNameProperty == null) 
+	   { 
+	       throw new RuntimeException("os.name property is not set"); 
+	   } 
+	   else 
+	   { 
+	       osNameProperty = osNameProperty.toLowerCase(); 
+	   } 
+
+	   if (osNameProperty.contains("win")) 
+	   { 
+	       return "win"; 
+	   } 
+	   else if (osNameProperty.contains("mac")) 
+	   { 
+	       return "osx"; 
+	   } 
+	   else if (osNameProperty.contains("linux") || osNameProperty.contains("nix")) 
+	   { 
+	       return "linux"; 
+	   } 
+	   else 
+	   { 
+	       throw new RuntimeException("Unknown OS name: " + osNameProperty); 
+	   } 
+	} 
+
+	public static String getArchName() 
+	{ 
+	   String osArch = System.getProperty("os.arch"); 
+
+	   if (osArch != null && osArch.contains("64")) 
+	   { 
+	       return "64"; 
+	   } 
+	   else 
+	   { 
+	       return "32"; 
+	   } 
+	}
+
+	public static void addJarToClasspath(File jarFile) 
+	{ 
+	   try 
+	   { 
+	       URL url = jarFile.toURI().toURL(); 
+	       URLClassLoader urlClassLoader = (URLClassLoader) ClassLoader.getSystemClassLoader(); 
+	       Class<?> urlClass = URLClassLoader.class; 
+	       Method method = urlClass.getDeclaredMethod("addURL", new Class<?>[] { URL.class }); 
+	       method.setAccessible(true);         
+	       method.invoke(urlClassLoader, new Object[] { url });             
+	   } 
+	   catch (Throwable t) 
+	   { 
+	       t.printStackTrace(); 
+	   }
+	}
 }
