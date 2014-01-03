bf204bc93b64a0fe9410c103c047f377d3fece76
Evert Chin <evertchin@gmail.com>
2014-01-03 14:16:51 +0800

    version pump

--- my/hearthtracking/app/HearthUI.java
+++ my/hearthtracking/app/HearthUI.java
@@ -128,7 +128,7 @@ public class HearthUI {
 	private static Image[] heroImgs;
 	private Composite composite_9;
 
-	public static int[] version = {1, 1, 4};
+	public static int[] version = {1, 1, 5};
 	public static int experimental = 0;
 	private Text txtWebSyncKey;
 	private Composite composite_11;
